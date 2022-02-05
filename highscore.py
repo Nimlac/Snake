25180613bb90d9eec421c30cf79acea652bb280a
Calvin <86289319+Nimlac@users.noreply.github.com>
2022-02-05 19:54:47 +0100

    Added cosmetics

--- highscore.py
+++ highscore.py
@@ -13,13 +13,26 @@ with open("snakedata.txt") as file:
     host = file.readline().strip("\n")
 cnx = mysql.connector.connect(user=uname, password=pw, host=host, database='calvin') 
 cursor=cnx.cursor(buffered=True)
-cursor.execute(("INSERT INTO highscore ""(name, score)""VALUES (%s, %s)"),(form.getvalue('name'),form.getvalue('score')))    
-cursor.execute("SELECT name, score FROM highscore ORDER BY score desc LIMIT 5")
+_name= form.getvalue('name')
+_score = form.getvalue('score')
+if _score != 0:
+    cursor.execute(("INSERT INTO highscore (name, score)VALUES (%s, %s)"),(_name,_score))    
+cursor.execute("SELECT name, score FROM highscore ORDER BY score desc LIMIT 7")
 a=[]
 for (name,score) in cursor:
     a.append((name,score))
+
+
+cursor.execute("SELECT max(score) FROM highscore where name = %s group by name",(_name,))
+for score in cursor:
+    sc=score
+
+cursor.execute("select count(*) + 1 as place from highscore where name<>%s and score > (select max(score) from highscore where name = %s group by name)",(_name,_name))
+for place in cursor:
+    pos=place
+
 print("Content-type: application/json\n")
-print(json.dumps(a))
+print(json.dumps({"scores":a, "best":(sc,pos)}))
 
 cursor.close()
 cnx.commit()
--- index.js
+++ index.js
@@ -1,50 +1,192 @@
 var images = [];
-function preload() {
-    for (var i = 0; i < arguments.length; i++) {
-        images[i] = new Image();
-        images[i].src = preload.arguments[i];
-    }
-}
+let color = "g";
+let appleCol = "r";
+let colorbg = "lime";
 
-document.cookie = "name=Player";
+let Snake;
+let FieldAv
+let score = 0
+let head;
+let apple;
+let next;
 
-
-// preload images to avoid lag while playing
-preload(
-    "sprites/apple.png",
-    "sprites/head_w.png",
-    "sprites/head_a.png",
-    "sprites/head_s.png",
-    "sprites/head_d.png",
-    "sprites/tail_w.png",
-    "sprites/tail_a.png",
-    "sprites/tail_s.png",
-    "sprites/tail_d.png",
-    "sprites/body_ww.png",
-    "sprites/body_aa.png",
-    "sprites/body_ss.png",
-    "sprites/body_wa.png",
-    "sprites/body_wd.png",
-    "sprites/body_aw.png",
-    "sprites/body_as.png",
-    "sprites/body_sa.png",
-    "sprites/body_sd.png",
-    "sprites/body_dw.png",
-    "sprites/body_ds.png"
-)
-
-
+let paused = false;
+let opensettings = false;
 const height = 14;
 const width = 16;
 
-
+let visInd = 0;
+let visArr = ["hidden", "visible"];
 let directions = [];
 let lastdir = 'd';
 
 let id = null;
+let wasPaused = false;
+
+function OnSubmitName() {
+    person = document.getElementById("name").value;
+    //person=document.forms["settings"]["name"].value;
+    if (person != null && person != "") {
+        color = "g";
+        var expiration_date = new Date();
+        expiration_date.setFullYear(expiration_date.getFullYear() + 1);
+        document.cookie = "name=" + person + "; path=/Snake; expires=" + expiration_date.toUTCString();
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "cgi-bin/highscore.py?name=" + person.replace(/&/g, "&amp").replace(/</g, "&lt").replace(/>/g, "&gt") + "&score=" + score, true);
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+
+                // Response
+                var response = JSON.parse(this.responseText);
+                document.getElementById("highscore").innerHTML = response.scores.join("<br>").replace(/,/g, ": ");
+                var best = response.best[0];
+                document.getElementById("personal_best").innerHTML = "Your best: " + best + "<br>Your place: " + response.best[1];
+                let ht = "<option value=\"green\">green</option>";
+                let ap = "<option value=\"red\">red</option>"
+                if (best >= 25) {
+                    document.getElementById("background").innerHTML = "<option value=\"lime\">lime</option><option value=\"blue\">blue</option><option value=\"dirt\">dirt</option><option value=\"desert\">desert</option><option value=\"black\">black</option><option value=\"white\">white</option>";
+                    if (best >= 36) {
+                        ht += "<option value=\"blue\">blue</option>"
+                        ap += "<option value=\"blue\">blue</option>"
+                        if (best >= 45) {
+                            ap += "<option value=\"gold\">gold</option>"
+                            if (best >= 50) {
+                                ht += "<option value=\"red\">red</option>"
+                                if (best >= 55) {
+                                    ap += "<option value=\"black\">black</option>"
+                                    if (best >= 60) {
+                                        ap += "<option value=\"pink\">pink</option>"
+                                        if (best >= 65) {
+                                            ht += "<option value=\"pink\">pink</option>"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else{
+                    document.getElementById("background").innerHTML = "<option value=\"lime\">lime</option>";
+                }
+                document.getElementById("type").innerHTML = ht;
+                document.getElementById("apple").innerHTML = ap;
+
+            }
+        };
+        xhttp.send();
+    }
+}
+
+function preload() {
+    document.getElementById("settings").style.visibility = "hidden";
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+    person = document.cookie.split(";").filter(x => x.startsWith("name="))[0].split("=")[1]
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "cgi-bin/highscore.py?name=" + person.replace(/&/g, "&amp").replace(/</g, "&lt").replace(/>/g, "&gt") + "&score=" + 0, true);
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // Response
+            var response = JSON.parse(this.responseText);
+            document.getElementById("highscore").innerHTML = response.scores.join("<br>").replace(/,/g, ": ");
+            if (person != "" && person != null && person != "Player")
+                document.getElementById("personal_best").innerHTML = "Your best: " + response.best[0] + "<br>Your place: " + response.best[1];
+
+        }
+    };
+    xhttp.send();
+    if (!document.cookie.match(/^(.*;)?\s*name\s*=\s*[^;]+(.*)?$/)) {
+        document.cookie = "name=Player";
+    }
+    document.getElementById("name").value = document.cookie.split(";").filter(x => x.startsWith("name="))[0].split("=")[1]
+    OnSubmitName();
+
+}
+
+
+// preload images to avoid lag while playin
+preload(
+    "sprites/r_apple.png",
+    "sprites/b_apple.png",
+    "sprites/g_apple.png",
+    //green
+    "sprites/g_head_w.png",
+    "sprites/g_head_a.png",
+    "sprites/g_head_s.png",
+    "sprites/g_head_d.png",
+    "sprites/g_tail_w.png",
+    "sprites/g_tail_a.png",
+    "sprites/g_tail_s.png",
+    "sprites/g_tail_d.png",
+    "sprites/g_body_ww.png",
+    "sprites/g_body_aa.png",
+    "sprites/g_body_ss.png",
+    "sprites/g_body_wa.png",
+    "sprites/g_body_wd.png",
+    "sprites/g_body_aw.png",
+    "sprites/g_body_as.png",
+    "sprites/g_body_sa.png",
+    "sprites/g_body_sd.png",
+    "sprites/g_body_dw.png",
+    "sprites/g_body_ds.png",
+    //blue
+    "sprites/b_head_w.png",
+    "sprites/b_head_a.png",
+    "sprites/b_head_s.png",
+    "sprites/b_head_d.png",
+    "sprites/b_tail_w.png",
+    "sprites/b_tail_a.png",
+    "sprites/b_tail_s.png",
+    "sprites/b_tail_d.png",
+    "sprites/b_body_ww.png",
+    "sprites/b_body_aa.png",
+    "sprites/b_body_ss.png",
+    "sprites/b_body_wa.png",
+    "sprites/b_body_wd.png",
+    "sprites/b_body_aw.png",
+    "sprites/b_body_as.png",
+    "sprites/b_body_sa.png",
+    "sprites/b_body_sd.png",
+    "sprites/b_body_dw.png",
+    "sprites/b_body_ds.png",
+    //red
+    "sprites/r_head_w.png",
+    "sprites/r_head_a.png",
+    "sprites/r_head_s.png",
+    "sprites/r_head_d.png",
+    "sprites/r_tail_w.png",
+    "sprites/r_tail_a.png",
+    "sprites/r_tail_s.png",
+    "sprites/r_tail_d.png",
+    "sprites/r_body_ww.png",
+    "sprites/r_body_aa.png",
+    "sprites/r_body_ss.png",
+    "sprites/r_body_wa.png",
+    "sprites/r_body_wd.png",
+    "sprites/r_body_aw.png",
+    "sprites/r_body_as.png",
+    "sprites/r_body_sa.png",
+    "sprites/r_body_sd.png",
+    "sprites/r_body_dw.png",
+    "sprites/r_body_ds.png"
+
+)
+
+
 
 document.addEventListener('keydown', function (event) {
+    if (opensettings || (paused && !"Pp+".includes(event.key)))
+        return;
     switch (event.key) {
+        case "P":
+        case "p": {
+            paused = !paused;
+            document.getElementById("gameboard").style.visibility = visArr[visInd];
+            visInd = 1 - visInd;
+            break;
+        }
         case "ArrowUp":
         case "W":
         case "w": {
@@ -72,6 +214,11 @@ document.addEventListener('keydown', function (event) {
         case "L":
         case "l": {
             init()
+            break;
+        }
+        case "+": {
+            OnOpenedSettings();
+            break;
         }
         default: {
             return;
@@ -80,30 +227,33 @@ document.addEventListener('keydown', function (event) {
     event.preventDefault();
 });
 
-let Snake;
-let FieldAv
-let score = 0
-let head;
-let apple;
-let next;
 
-function death(){
-    let person = prompt("YOU LOST SCORE: " + score + "\nPlease enter your name:", document.cookie.split(";").filter(x=> x.startsWith("name="))[0].split("=")[1]);
+
+
+
+
+function death() {
+    let person = prompt("YOU LOST SCORE: " + score + "\nPlease enter your name:", document.cookie.split(";").filter(x => x.startsWith("name="))[0].split("=")[1]);
     if (person != null && person != "") {
-        document.cookie = "name="+person;
+        document.getElementById("name").value = person;
+        var expiration_date = new Date();
+        expiration_date.setFullYear(expiration_date.getFullYear() + 1);
+        document.cookie = "name=" + person + "; path=/Snake; expires=" + expiration_date.toUTCString();
         var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "cgi-bin/highscore.py?name="+person.replace(/&/g,"&amp").replace(/</g,"&lt").replace(/>/g,"&gt")+"&score="+score, true);
-        xhttp.onreadystatechange = function() {
-           if (this.readyState == 4 && this.status == 200) {
-        
+        xhttp.open("GET", "cgi-bin/highscore.py?name=" + person.replace(/&/g, "&amp").replace(/</g, "&lt").replace(/>/g, "&gt") + "&score=" + score, true);
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+
                 // Response
                 var response = JSON.parse(this.responseText);
-                document.getElementById("highscore").innerHTML=response.join("<br>").replace(/,/g,": ");
-           }
+                document.getElementById("highscore").innerHTML = response.scores.join("<br>").replace(/,/g, ": ");
+                document.getElementById("personal_best").innerHTML = "Your best: " + response.best[0] + "<br>Your place: " + response.best[1];
+
+            }
         };
         xhttp.send();
     }
-    init()
+    init();
 }
 
 
@@ -113,23 +263,31 @@ function init() {
     for (let y = 0; y < 15; y++) {
         for (let x = 0; x < 17; x++) {
             FieldAv.push([y, x]);
-            document.getElementById(y + ":" + x).setAttribute("style", "background-color: lime;");
+            document.getElementById(y+":"+x).style.background=colorbg;
+            //document.getElementById(y+":"+x).style.backgroundImage="none";
         }
     }
-    lastdir = 'd';
 
+    lastdir = 'd';
+    paused = false;
     directions = [];
 
-    document.getElementById("7:2").setAttribute("style", "background-image: url(sprites/tail_d.png);");
-    document.getElementById("7:3").setAttribute("style", "background-image: url(sprites/body_dd.png);");
-    document.getElementById("7:4").setAttribute("style", "background-image: url(sprites/head_d.png);");
+
+    document.getElementById("7:2").style.background= (colorbg+" url(sprites/" + color + "_tail_d.png) no-repeat center");
+    document.getElementById("7:2").style.backgroundSize="cover";
+    document.getElementById("7:3").style.background= (colorbg+" url(sprites/" + color + "_body_dd.png) no-repeat center");
+    document.getElementById("7:3").style.backgroundSize="cover";
+    document.getElementById("7:4").style.background= (colorbg+" url(sprites/" + color + "_head_d.png) no-repeat center");
+    document.getElementById("7:4").style.backgroundSize="cover";
 
 
     FieldAv = FieldAv.filter(x => x[1] != 2 || x[0] != 7);
     FieldAv = FieldAv.filter(x => x[1] != 3 || x[0] != 7);
     FieldAv = FieldAv.filter(x => x[1] != 4 || x[0] != 7);
 
-    document.getElementById("7:12").setAttribute("style", "background-image: url(sprites/apple.png);");
+    document.getElementById("7:12").style.background= (colorbg+" url(sprites/" + appleCol + "_apple.png) no-repeat center");
+    document.getElementById("7:12").style.backgroundSize="cover";
+
     if (apple != null) {
         document.getElementById(apple[0] + ":" + apple[1]).innerHTML = " ";
     }
@@ -145,10 +303,122 @@ function init() {
     id = window.setInterval(play, 300);
 }
 
+function OnOpenedSettings() {
+    document.getElementById("gameboard").style.visibility = "hidden";
+    document.getElementById("score").innerHTML = "";
+    paused = true;
+    opensettings = true;
+    document.getElementById("settings").setAttribute("style", "visibility: visible;");
+}
 
+function OnClosedSettings() {
+    OnSubmitApple();
+    OnSubmitBackground();
+    OnSubmitType();
+    document.getElementById("gameboard").style.visibility = "visible";
+    document.getElementById("score").innerHTML = score;
+    paused = false;
+    opensettings = false;
+    document.getElementById("settings").setAttribute("style", "visibility: hidden;");
+    visInd = 0;
+}
+
+
+
+
+
+function OnSubmitType() {
+    switch (document.getElementById("type").value) {
+        case "green": {
+            color = "g"
+            break;
+        }
+        case "blue": {
+            color = "b"
+            break;
+        }
+        case "red": {
+            color = "r"
+            break;
+        }
+        default: {
+            return
+        }
+    }
+}
+
+
+function OnSubmitApple() {
+    switch (document.getElementById("apple").value) {
+        case "red": {
+            appleCol = "r"
+            break;
+        }
+        case "blue": {
+            appleCol = "b"
+            break;
+        }
+        case "gold": {
+            appleCol = "g"
+            break;
+        }
+        case "black": {
+            appleCol = "d"
+            break;
+        }
+        case "pink": {
+            appleCol = "p"
+            break;
+        }
+        default: {
+            return
+        }
+    }
+    document.getElementById(apple[0]+":"+apple[1]).style.background= (colorbg+" url(sprites/" + appleCol + "_apple.png) no-repeat center");
+    document.getElementById(apple[0]+":"+apple[1]).style.backgroundSize="cover";
+}
+
+function OnSubmitBackground() {
+    switch (document.getElementById("background").value) {
+        case "lime": {
+            colorbg = "lime"
+            break;
+        }
+        case "blue": {
+            colorbg = "blue"
+            break;
+        }
+        case "dirt": {
+            colorbg = "brown"
+            break;
+        }
+        case "desert": {
+            colorbg = "sandybrown"
+            break;
+        }
+        case "black": {
+            colorbg = "black"
+            break;
+        }
+        case "white": {
+            colorbg = "white"
+            break;
+        }
+        default: {
+            return
+        }
+    }
+    for (let y = 0; y < 15; y++) {
+        for (let x = 0; x < 17; x++) {
+            document.getElementById(y + ":" + x).style.backgroundColor=colorbg;
+        }
+    } 
+}
 
 
 function play() {
+    if (paused)
+        return;
 
     if (directions.length == 0) {
         directions.push(lastdir)
@@ -233,8 +503,8 @@ function play() {
             }
 
     }
-    document.getElementById(head[0] + ":" + head[1]).setAttribute("style", "background-image: url(sprites/body_" + lastdir + directions[0] + ".png);");
-
+    document.getElementById(head[0] + ":" + head[1]).style.background=(colorbg+" url(sprites/" + color + "_body_" + lastdir + directions[0] + ".png) no-repeat center");
+    document.getElementById(head[0] + ":" + head[1]).style.backgroundSize="cover";
     lastdir = directions[0];
     directions.shift()
 
@@ -244,7 +514,8 @@ function play() {
     }
     snake.push([next[0], next[1], lastdir]);
 
-    document.getElementById(next[0] + ":" + next[1]).setAttribute("style", "background-image: url(sprites/head_" + lastdir + ".png);");
+    document.getElementById(next[0] + ":" + next[1]).style.background=(colorbg+" url(sprites/" + color + "_head_" + lastdir + ".png) no-repeat center");
+    document.getElementById(next[0] + ":" + next[1]).style.backgroundSize="cover";
 
 
     FieldAv = FieldAv.filter(x => x[1] != next[1] || x[0] != next[0]);
@@ -257,7 +528,8 @@ function play() {
 
         document.getElementById("score").innerHTML = score.toString();
         let co = FieldAv[Math.floor(Math.random() * (FieldAv.length))];
-        document.getElementById(co[0] + ":" + co[1]).setAttribute("style", "background-image: url(sprites/apple.png);");
+        document.getElementById(co[0] + ":" + co[1]).style.background=(colorbg+" url(sprites/" + appleCol + "_apple.png) no-repeat center");
+        document.getElementById(co[0] + ":" + co[1]).style.backgroundSize="cover";
         apple = co;
         if (score == 15) {
             window.clearInterval(id);
@@ -277,12 +549,14 @@ function play() {
         }
     }
     else {
-        document.getElementById(snake[1][0] + ":" + snake[1][1]).setAttribute("style", "background-image: url(sprites/tail_" + snake[2][2] + ".png);");
+        document.getElementById(snake[1][0] + ":" + snake[1][1]).style.background= (colorbg+" url(sprites/" + color + "_tail_" + snake[2][2] + ".png) no-repeat center");
+        document.getElementById(snake[1][0] + ":" + snake[1][1]).style.backgroundSize="cover";
+
         let l = snake[0];
-        document.getElementById(l[0] + ":" + l[1]).setAttribute("style", "background-image: none;");
+        document.getElementById(l[0] + ":" + l[1]).setAttribute("style", "background: "+colorbg+";");
         FieldAv.push(snake.shift());
     }
 }
 
-alert("Snake will start when closing this popup!\nThe speed of the game will increase over time.\nControl with WASD or arrow keys.\nYou can reload the game by presing L")
+alert("Snake will start when closing this popup!\nThe speed of the game will increase over time.\nControl with WASD or arrow keys.\nYou can reload the game by presing L\nYou can pause by pressing P\nPress + to open the settings")
 init()
